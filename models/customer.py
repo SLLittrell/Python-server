6f30d6df79ffd8efd5409c14e57247f4889cae58
sllittrell <stacey.littrell@gmail.com>
2021-04-12 10:15:04 -0500

    created classes and attributes for all data

---
+++ models/customer.py
@@ -0,0 +1,5 @@
+class Customer():
+    def __init__(self, id, email, name)
+        self.id = id
+        self.email = email
+        self.name = name
---
+++ models/employee.py
@@ -0,0 +1,10 @@
+class Employee():
+    def __init__(self, id, name, locationId)
+        self.id = id
+        self.name = name 
+        self.locationId = locationId
+
+
+
+
+
