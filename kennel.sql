1c20f855e62cf54a2e219363762bee222d8e0249
sllittrell <stacey.littrell@gmail.com>
2021-04-14 13:30:27 -0500

    Refactor delete for animal component

--- kennel.sql
+++ kennel.sql
@@ -53,7 +53,7 @@ INSERT INTO `Employee` VALUES (null, "Meg Ducharme", "404 Unknown Ct", 2);
 INSERT INTO `Employee` VALUES (null, "Hannah Hall", "204 Empty Ave", 1);
 INSERT INTO `Employee` VALUES (null, "Leah Hoefling", "200 Success Way", 2);
 
-SELECT * FROM 'Customer'
+SELECT * FROM 'Animal'
 
 SELECT
     a.id,
